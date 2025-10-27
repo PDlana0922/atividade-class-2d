95c214542d2702cd6d3946cedec58d905e121c9c
PDlana0922 <lana.depin@escola.pr.gov.br>
2025-10-27 20:23:26 -0300

    Add files via upload

---
+++ f.py
@@ -0,0 +1,8 @@
+# Aluna: Lana
+# Programa que lê um número real e mostra o dobro e a terça parte.
+
+n = float(input("Digite um número: "))
+dobro = n * 2
+terca = n / 3
+print(f"O dobro de {n} é {dobro:.2f}.")
+print(f"A terça parte de {n} é {terca:.5f}.")
