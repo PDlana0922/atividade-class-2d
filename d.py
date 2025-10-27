58fa196bfd47003d4d241363e3b39f74ca9a7537
PDlana0922 <lana.depin@escola.pr.gov.br>
2025-10-27 20:21:58 -0300

    Add files via upload

---
+++ d.py
@@ -0,0 +1,7 @@
+# Aluna: Lana
+# Programa que lê duas notas de um aluno e mostra sua média.
+
+nota1 = float(input("Nota 1: "))
+nota2 = float(input("Nota 2: "))
+media = (nota1 + nota2) / 2
+print(f"A média entre {nota1} e {nota2} é igual a {media:.1f}.")
