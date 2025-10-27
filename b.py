b85f094daea41b93bdf8767d02b9c05a90d11315
PDlana0922 <lana.depin@escola.pr.gov.br>
2025-10-27 20:20:27 -0300

    Add files via upload

---
+++ b.py
@@ -0,0 +1,6 @@
+# Aluna: Lana
+# Programa que lê o nome e o salário de um funcionário e mostra uma mensagem final.
+
+nome = input("Nome do funcionário: ")
+salario = float(input("Salário: "))
+print(f"Funcionário {nome} tem um salário de R${salario:.2f} em Junho.")
