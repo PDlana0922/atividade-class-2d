0f0218d13dda094efec5e737a823967718547ef7
PDlana0922 <lana.depin@escola.pr.gov.br>
2025-10-27 20:23:46 -0300

    Add files via upload

---
+++ g.py
@@ -0,0 +1,11 @@
+# Aluna: Lana
+# Programa que lê uma distância em metros e mostra em outras medidas.
+
+m = float(input("Digite uma distância em metros: "))
+print(f"A distância de {m}m corresponde a:")
+print(f"{m/1000}Km")
+print(f"{m/100}Hm")
+print(f"{m/10}Dam")
+print(f"{m*10}dm")
+print(f"{m*100}cm")
+print(f"{m*1000}mm")
