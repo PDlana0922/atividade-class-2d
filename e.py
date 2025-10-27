1a7a6643404a9f19d0b15bf649c79ac5bc05c742
PDlana0922 <lana.depin@escola.pr.gov.br>
2025-10-27 20:22:37 -0300

    Add files via upload

---
+++ e.py
@@ -0,0 +1,6 @@
+# Aluna: Lana
+# Programa que lê um número inteiro e mostra seu antecessor e sucessor.
+
+n = int(input("Digite um número: "))
+print(f"O antecessor de {n} é {n-1}.")
+print(f"O sucessor de {n} é {n+1}.")
